9cd624ec283c1f7833b657c8fde782f908a6a840
Rowan2005 <58427428+Rowan2005@users.noreply.github.com>
2020-07-21 20:36:10 +0530

    Update CloudStorage.py

--- CloudStorage.py
+++ CloudStorage.py
@@ -1,12 +1,18 @@
 import dropbox
+import os
 
 class TransferData:
     def __init__(self,accesstoken):
         self.accesstoken = accesstoken
     def uploadfile(self,file_from,file_to):
         dbx = dropbox.Dropbox(self.accesstoken)
-        f = open(file_from,'rb')
-        dbx.files_upload(f.read(),file_to)
+        for root ,dirs,files in os.walk(file_from):
+            for filename in files:
+                local_path = os.path.join(root,filename)
+                relative_path = os.path.relpath(local_path,file_from)
+                dropbox_path = os.path.join(file_to, relative_path)
+                with open(local_path,'rb') as f:
+                    dbx.files_upload(f.read(),dropbox_path,mode = WriteMode('overwrite'))
 def main():
     accesstoken = 'iD8Xi0tCt4AAAAAAAAAAGKjuGT6Y7w3cJz3vnO9VrlXOGzGNFxHWyMBf4j0AqyO9'
     transferData = TransferData(accesstoken)
